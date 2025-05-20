b057a410de6ac689d752e956225b7da0ff4ac54a
leliz-git <leli202654@gmail.com>
2025-05-20 15:51:29 +0300

    20/05/2025

--- MyShopWebApi/Program.cs
+++ MyShopWebApi/Program.cs
@@ -9,18 +9,40 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserService , UserService>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 //builder.Services.AddOpenApi();
 builder.Services.AddDbContext<MyShop_327882650Context>(options =>
     options.UseSqlServer("Data Source=SRV2\\PUPILS;Initial Catalog=MyShop_327882650 ; Integrated Security=True; Trusted_Connection=True;TrustServerCertificate=True"));
 
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
 var app = builder.Build();
-
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "My API V1");
+    });
+}
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
 //    app.MapOpenApi();
 //}
+
+
+
+
+// Configure the HTTP request pipeline.
+
 
 app.UseHttpsRedirection();
 
--- Services/IProductService.cs
+++ Services/IProductService.cs
@@ -4,6 +4,6 @@ namespace Services
 {
     public interface IProductService
     {
-        Task<List<Product>> GetProducts()
+        Task<List<Product>> GetProducts();
     }
 }
